20eb23a406e311a1cccc1ba83b4e0295112f6690
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-09-07 21:30:00 -0400

    add connection to the server to handle key pressed

--- input.js
+++ input.js
@@ -1,24 +1,8 @@
+let connection;
 
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') { // Check for Ctrl + C 
-    console.log("Exiting the game.");
-    process.exit();
-  } else if (key === '\u001B[A') { // Up arrow key
-    console.log("Up arrow key pressed");
-    // Handle up arrow action here
-  } else if (key === '\u001B[B') { // Down arrow key
-    console.log("Down arrow key pressed");
-    // Handle down arrow action here
-  } else if (key === '\u001B[C') { // Right arrow key
-    console.log("Right arrow key pressed");
-    // Handle right arrow action here
-  } else if (key === '\u001B[D') { // Left arrow key
-    console.log("Left arrow key pressed");
-  }
-};
-
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -26,6 +10,26 @@ const setupInput = function () {
   stdin.on("data", handleUserInput);
   return stdin;
 };
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') { // Check for Ctrl + C
+    console.log("Exiting the game.");
+    process.exit();
+  } else if (key === 'w') { // 'w' key
+    console.log("Move: up");
+    connection.write("Move: up")
+   
+  } else if (key === 'a') { // 'a' key
+    console.log("Move: left");
+    connection.write("Move: left")
+  } else if (key === 's') { // 's' key
+    console.log("Move: down");
+    connection.write("Move: down")
+  } else if (key === 'd') { // 'd' key
+    console.log("Move: right");
+    connection.write("Move: right")
+  }
+}
 
 
 module.exports = {
